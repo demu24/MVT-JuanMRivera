0f8f5a1f233ac5a97ee2e47d5a45e0d818e6015e
Juan Manuel <jmr092@gmail.com>
2022-06-26 19:42:42 -0300

    Me falto un nombre

--- Familiares/views.py
+++ Familiares/views.py
@@ -9,7 +9,7 @@ from .models import Familiares
 def principal(request):
 
     familiar1 = Familiares(cedula=25554447,nombre='Miguelo',fecha_nacimiento='07/11/1999')
-    familiar2 = Familiares(cedula=34446669,nombre='',fecha_nacimiento='04/11/1950')
+    familiar2 = Familiares(cedula=34446669,nombre='Sebestien',fecha_nacimiento='04/11/1950')
     familiar3 = Familiares(cedula=53222247,nombre='Mercio',fecha_nacimiento='12/12/1932')
 
     lista_familiares = [familiar1, familiar2, familiar3]
